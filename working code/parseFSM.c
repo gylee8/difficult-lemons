83fac6b6c9133acfb13a4682060262e00c6ffd6d
caitlin <cyl11603@gmail.com>
2022-03-24 00:33:58 -0400

    Added some code to parse the type and number of states from text file

--- working code/parseFSM.c
+++ working code/parseFSM.c
@@ -5,16 +5,30 @@
 // Auxiliary function to count the number of lines in the file
 int numLines(FILE *f);
 
+// Convert int representing octal into normal binary
+char octToBin(int i);
+
+// Read information from config.txt
+void readTable(char *table);
+
 int main(){
+  printf("010 = %d", octToBin(10000000));
+}
+
+void readTable(char *type, char *states, char **table){
   FILE *file;
   file = fopen("config.txt", "r");
 
   int lines = numLines(file);
 
-  int type = 0;
-  int states = 0;
-  fscanf(file, "%d", &type);
-  fscanf(file, "%d", &states);  // Need to change this to parse a binary value
+  int num = 0;
+  fscanf(file, "%o", &num);
+  *type = octToBin(num);
+  fscanf(file, "%o", &num);
+  *states = octToBin(num);
 
-  int nums[numLines][5 - type]  // Assuming Mealy = 1, Moore = 0
+}
+
+char octToBin(int i){
+  return ((i >> 11) & 1)*4 + ((i >> 7) & 1)*2 + ((i >> 3) & 1);
 }
