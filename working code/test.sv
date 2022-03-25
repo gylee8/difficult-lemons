61097b0fdda2c6a7712ac0a047a0f1aae8fd23c6
Gillian Lee <gillianylee@gmail.com>
2022-03-25 10:15:54 -0400

    1st pass for code to parse transition table from 1D array

--- working code/test.sv
+++ working code/test.sv
@@ -15,8 +15,26 @@ class stateTr;
   endfunction
 endclass
 
+class randInput;
+  rand byte arr[];
+
+  constraint c_size{arr.size() inside {[5:10]};}
+  constraint c_data{foreach(arr[i])
+                      arr[i] inside {[0:3]};}
+
+  function new();
+    arr = new[5];
+  endfunction
+
+  function void print();
+    $display("array size: %0d", arr.size());
+    $display("printing elements in array");
+    $display("%p", arr);
+  endfunction
+endclass
+
 module test();
-  //call parseFSM.c --> parseFSM.c returns 2D array of all text file info
+  //call parseFSM.c --> parseFSM.c returns 1D array of all text file info
   //parseFSM should return an object because otherwise, the number of states can't be obtained since there's no way to define the array on the lefthand side of the equals sign
   //stateTr class --> defines objects to store in 2D array of state transitions
 
@@ -24,23 +42,39 @@ module test();
   int i,j;
   byte numStates;
   byte fsmType; //0 = mealy, 1 = moore
-  byte arr [5:0][4:0]; //stores raw state transition table
+  byte arr [19:0]; //stores raw state transition table
   stateTr [2:0] transitions [];
+  randInput inputs;
+
+  import "DPI-C" function void readTable(output byte fsmType, output byte numStates, output byte arr[])
 
   initial begin
-    //pass numStates, initialState, arr into function call
+    //parse transition table
+    readTable(fsmType, numStates, arr);
     transitions = new[numStates]; //set size of dynamic array
-    if (fsmType == 1) begin //parse transitions for moore machine
+    if (fsmType == 0) begin //parse transitions for mealy machine
       for (i=0; i<numStates; i++) begin
         transitions[i][0] = new(); //corresponding to input=0
-        transitions[i][0].setNextState(arr[i][1]);
-        transitions[i][0].setOutput(arr[i][3]);
+        transitions[i][0].setNextState(arr[5*i+1]);
+        transitions[i][0].setOutput(arr[5*i+2]);
 
         transitions[i][1] = new(); //corresponding to input=1
-        transitions[i][1].setNextState(arr[i][2]);
-        transitions[i][1].setOutput(arr[i][3]);
+        transitions[i][1].setNextState(arr[5*i+3]);
+        transitions[i][1].setOutput(arr[5*i+4]);
+      end
+    end else if (fsmType == 1) begin //parse transitions for moore machine
+      for (i=0; i<numStates; i++) begin
+        transitions[i][0] = new(); //corresponding to input=0
+        transitions[i][0].setNextState(arr[4*i+1]);
+        transitions[i][0].setOutput(arr[4*i+3]);
+
+        transitions[i][1] = new(); //corresponding to input=1
+        transitions[i][1].setNextState(arr[i][4*i+2]);
+        transitions[i][1].setOutput(arr[i][4*i+3]);
       end
     end
+
+    
 
   end
 endmodule
