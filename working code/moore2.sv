a1e9d3099a0256fa8a49130561e8ef56e320862e
caitlin <cyl11603@gmail.com>
2022-03-27 18:12:57 -0400

    updates to moore2.sv

--- working code/moore2.sv
+++ working code/moore2.sv
@@ -2,39 +2,46 @@ module moore2(
   input wire clk, reset,
   input wire [1:0] sw_in,
   input wire ctrl_in,
-  output reg state
+  output reg state, out
   );
 
-  reg next;
+  reg next, out_int;
 
-  always @(posedge clk and (posedge reset or ctrl_in)) begin
+  parameter s1 = 1'b0;
+  parameter s2 = 1'b1;
+
+  always @(posedge clk or ctrl_in) begin
     if(reset) begin
       state <= 0;
     end
     else if (ctrl_in) begin
       state <= next;
+      out <= out_int;
     end
   end
 
   always @(sw_in, state) begin
     next = state;
+    out_int = 0;
     case(state)
-      0: begin
+      s1: begin
         if(sw_in > 0) begin
           next = 1;
         end
         else begin
           next = 0;
         end
+        out_int = 1;
       end
 
-      1: begin
-        if(sw_in == 0 or sw_in == 2) begin
+      s2: begin
+        if(sw_in == 0 || sw_in == 2) begin
           next = 1;
         end
         else begin
           next = 0;
         end
+        out_int = 0;
       end
     endcase
   end
