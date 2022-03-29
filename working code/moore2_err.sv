d8e41bbade53f43b2bb4cbedf7268707def221cb
caitlin <cyl11603@gmail.com>
2022-03-29 16:18:33 -0400

    new moore2: 2 state moore with no errors (prob); moore2_err is v broken

--- working code/moore2_err.sv
+++ working code/moore2_err.sv
@@ -3,22 +3,20 @@ module moore2(
   input wire [1:0] sw_in,
   input wire ctrl_in,
   input wire state_in,
-  output reg state,
-  output reg out
+  output reg state, out
   );
 
   reg next, out_int;
 
+  parameter s1 = 1'b0;
+  parameter s2 = 1'b1;
+
   initial begin
-    #5
-    //$display("initial begin: state <= state_in");
-    state = state_in;
+    state <= state_in;
   end
 
-  always @(posedge clk) begin
-    // $display("posedge clk");
+  always @(posedge clk or ctrl_in) begin
     if(reset) begin
-      // $display("reset");
       state <= 0;
     end
     else if (ctrl_in) begin
@@ -29,8 +27,9 @@ module moore2(
 
   always @(sw_in, state) begin
     next = state;
+    out_int = 0;
     case(state)
-      0: begin
+      s1: begin
         if(sw_in > 0) begin
           next = 1;
           out_int = 1;
@@ -41,7 +40,7 @@ module moore2(
         end
       end
 
-      1: begin
+      s2: begin
         if(sw_in == 0 || sw_in == 2) begin
           next = 1;
           out_int = 1;
