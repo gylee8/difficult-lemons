ce5d4ad0f4d3d58024b8243c6ee6917e8959807f
Gillian Lee <gillianylee@gmail.com>
2022-03-29 21:45:32 -0400

    Various changes made to test in Vivado

--- working code/test.sv
+++ working code/test.sv
@@ -15,7 +15,7 @@ class randInputs;
   rand byte SWInputArr[];
   rand byte CtrInputArr[];
 
-  constraint c_sizeSW{SWInputArr.size() inside {[10:20]};}
+  constraint c_sizeSW{SWInputArr.size() inside {[20:30]};}
   constraint c_dataSW{foreach(SWInputArr[i])
                         SWInputArr[i] inside {[0:3]};}
   constraint c_sizeCtr{CtrInputArr.size() == SWInputArr.size();}
@@ -56,6 +56,10 @@ module test();
   //stateTr class --> defines objects to store in 2D array of state transitions
 
   int i,j;
+  int totalErrCount, numTransitions; //total number of errors and number of attempted transitions
+  int outErrCount; //number of output errors
+  int nextErrCount; //number of incorrect next states
+  int numStErrCount; //number of times the incorrect number of states was detected
   byte numStates, curState;
   logic curOut;
   byte fsmType; //0 = mealy, 1 = moore
@@ -80,6 +84,11 @@ module test();
   initial begin
     reset = 0;
     clk = 0;
+    totalErrCount = 0;
+    numTransitions = 0;
+    outErrCount = 0;
+    nextErrCount = 0;
+    numStErrCount = 0;
     forever #1s clk=~clk;
   end
 
@@ -199,6 +208,12 @@ module test();
       ctrIn = inputArr.CtrInputArr[i]; //input accepted when ctr_input is HIGH
       $display("CTR: %0d", ctrIn);
     end
+    $display();
+    $display("----------------------------");
+    $display("Total Errors: %0d", numStErrCount+nextErrCount+outErrCount);
+    $display("# times incorrect number of states was detected: %0d", numStErrCount);
+    $display("# times next state was incorrect: %0d", nextErrCount);
+    $display("# times output was incorrect: %0d", outErrCount);
     $finish;
   end
 
@@ -216,23 +231,26 @@ module test();
       end
     end
 
+    #10ns;
     $display("expected state = %0d, output = %0d", curState, curOut);
     $display("DUT      state = %0d, output = %0d", DUTcurState, DUTout);
-
     if (curState != DUTcurState || curOut != DUTout) begin //compare to DUT
       $display("  ###############  ");
       if (curState != DUTcurState) begin
         if (DUTcurState > numStates-1) begin
+          numStErrCount++;
           $display("ERROR--incorrect number of states");
           $display(" max state: %0d", numStates-1);
           $display(" DUT state: %0d", DUTcurState);
         end else begin
+          nextErrCount++;
           $display("ERROR--incorrect next state");
           $display(" expected : %0d", curState);
           $display(" DUT state: %0d", DUTcurState);
         end
       end
       if (curOut != DUTout) begin
+        outErrCount++;
         $display("ERROR--incorrect output");
         $display(" expected  : %0d", curOut);
         $display(" DUT output: %0d", DUTout);
